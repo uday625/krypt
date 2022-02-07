3bd8b6b2b5be95e359aa7be1b195864459957367
Uday Bag <uday625@Nemesis.phub.net.cable.rogers.com>
2022-02-07 00:36:09 -0500

    added Transactions SC

---
+++ smart_contract/contracts/Transactions.sol
@@ -0,0 +1,39 @@
+//SPDX-License-Identifier: UNLICENSED
+
+pragma solidity ^0.8.0;
+
+contract Transactions{
+
+    uint256 transactionCount;
+
+    event Transfer(address from, address receiver, 
+                    uint amount, string message, 
+                    uint256 timestamp, string keyword);
+
+    struct TransferStruct{
+        address sender;
+        address receiver;
+        uint amount;
+        string message;
+        uint256 timestamp;
+        string keyword;
+    }
+
+    TransferStruct [] transactions;
+
+    function addToBlockchain(address payable _receiver, uint _amount, string memory _message, string memory _keyword) public {
+        transactionCount +=1;
+        transactions.push(TransferStruct(msg.sender, _receiver, _amount, _message, block.timestamp, _keyword));
+        emit Transfer(msg.sender, _receiver, _amount, _message, block.timestamp, _keyword);
+    }
+
+    function getAlltransaction() public view returns (TransferStruct[] memory){
+        return transactions;
+    }
+
+    function getTransactionCount() public view returns (uint256) {
+        return transactionCount;
+    }
+
+
+}
